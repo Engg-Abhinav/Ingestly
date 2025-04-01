7e3a6789d643fd2fb637abb7dcf6a0b94a3e2d13
Abhinav Agrawal <104521563+Engg-Abhinav@users.noreply.github.com>
2025-04-02 00:04:27 +0530

    Ingestly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,20 @@ function App() {
     setError('');
     setAnswer('');
     try {
-      const response = await fetch('http://127.0.0.1:5000/ask', {
+      // For local
+      // const response = await fetch('http://127.0.0.1:5000/ask', {
+      //   method: 'POST',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   body: JSON.stringify({ question, content })
+      // });
+
+      // For render
+      const response = await fetch('https://ingestly.onrender.com/ask', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ question, content })
       });
+      
       const data = await response.json();
 
       if (response.ok) {
